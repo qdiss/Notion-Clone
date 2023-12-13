ab1c3a878c1ceca2f60e205740b06db536412fb8
Adis <klobodanovic.adis.21@size.ba>
2023-12-13 17:45:12 +0100

    Editor

--- app/(main)/_components/item.tsx
+++ app/(main)/_components/item.tsx
@@ -59,7 +59,9 @@ export const Item = ({
   const onArchive = (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
     event.stopPropagation();
     if (!id) return;
-    const promise = archive({ id });
+    const promise = archive({ id }).then(() => {
+      router.push("/documents");
+    });
 
     toast.promise(promise, {
       loading: "Moving to trash...",
@@ -84,7 +86,7 @@ export const Item = ({
         if (!expanded) {
           onExpand?.();
         }
-        // router.push(`/documents/${documentId}`);
+        router.push(`/documents/${documentId}`);
       }
     );
 
@@ -121,7 +123,10 @@ export const Item = ({
       {documentIcon ? (
         <div className="shrink-0 mr-2 text-[18px]">{documentIcon}</div>
       ) : (
-        <Icon className="shrink-0 h-[18px] mr-2 text-muted-foreground" />
+        <Icon
+          className="shrink-0 h-[18px] h
+        w-[18px] mr-2 text-muted-foreground"
+        />
       )}
       <span className="truncate">{label}</span>
       {isSearch && (
--- components/cover.tsx
+++ components/cover.tsx
@@ -11,6 +11,7 @@ import { useCoverImage } from "@/hooks/use-cover-image";
 import { api } from "@/convex/_generated/api";
 import { Id } from "@/convex/_generated/dataModel";
 import { useEdgeStore } from "@/lib/edgestore";
+import { Skeleton } from "./ui/skeleton";
 
 interface CoverImageProps {
   url?: string;
@@ -68,4 +69,8 @@ export const Cover = ({ url, preview }: CoverImageProps) => {
       )}
     </div>
   );
+};
+
+Cover.Skeleton = function CoverSkeleton() {
+  return <Skeleton className="w-full h-[12vh]" />;
 };
